7cd759dbd4bb7fe66dd4e96866aa433478e2b4f1
k-n195 <150040736+k-n195@users.noreply.github.com>
2025-12-27 02:27:10 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,2 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOTN78VOU1afvzaJSnc0owUgof5YkZR5Q",
+  authDomain: "hangman-multiplayer-207f7.firebaseapp.com",
+  projectId: "hangman-multiplayer-207f7",
+  storageBucket: "hangman-multiplayer-207f7.firebasestorage.app",
+  messagingSenderId: "371709670932",
+  appId: "1:371709670932:web:30d2924bfe233a9e2bd901",
+  measurementId: "G-HM15FKC9X9",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
